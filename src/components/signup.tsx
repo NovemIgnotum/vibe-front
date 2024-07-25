d4df48b9afab81a4ca24d18a7ac83a34fae9ccd5
Baptiste <102555318+NovemIgnotum@users.noreply.github.com>
2024-07-25 13:39:58 +0200

    Mise en place du formulaire en deux partie

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
+import "./style/signup.css";
 
 const Signup = () => {
+  const [stepCounter, setStepCounter] = useState(1);
   const [formValues, setFormValues] = useState({
     name: "",
     firstName: "",
     email: "",
     password: "",
     passwordConfirm: "",
-    adress: {
+    address: {
       street: "",
       city: "",
       zipCode: "",
@@ -15,33 +17,161 @@ const Signup = () => {
     pseudo: "",
   });
 
-  return (
+  const createUser = async () => {
+    console.log("User created");
+    console.log(formValues);
+    setFormValues({
+      name: "",
+      firstName: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+      address: {
+        street: "",
+        city: "",
+        zipCode: "",
+      },
+      pseudo: "",
+    });
+  };
+
+  return stepCounter === 1 ? (
     <div
       style={{
-        backgroundColor: "RED",
+        backgroundColor: "red",
         height: "800px",
         display: "flex",
         flexDirection: "column",
         width: "800px",
+        alignItems: "flex-start",
       }}
     >
       <label htmlFor="Nom">Nom</label>
-      <input type="text" id="Nom" name="Nom" />
-      <label htmlFor="Prénom">Prénom</label>
-      <input type="text" id="Prénom" name="Prénom" />
-      <label htmlFor="Email">Email</label>
-      <input type="text" id="Email" name="Email" />
-      <label htmlFor="Mot de passe">Mot de passe</label>
-      <input type="password" id="Mot de passe" name="Mot de passe" />
-      <label htmlFor="Confirmer mot de passe">Confirmer mot de passe</label>
       <input
-        type="password"
-        id="Confirmer mot de passe"
-        name="Confirmer mot de passe"
+        className="form-input"
+        type="text"
+        id="Nom"
+        name="Nom"
+        value={formValues.name}
+        onChange={(e) => setFormValues({ ...formValues, name: e.target.value })}
       />
-      <button>Suivant </button>
+      <label htmlFor="Prénom">Prénom</label>
+      <input
+        className="form-input"
+        type="text"
+        id="Prénom"
+        name="Prénom"
+        value={formValues.firstName}
+        onChange={(e) =>
+          setFormValues({ ...formValues, firstName: e.target.value })
+        }
+      />
+      <label htmlFor="Email">Email</label>
+      <input
+        className="form-input"
+        type="text"
+        id="Email"
+        name="Email"
+        value={formValues.email}
+        onChange={(e) =>
+          setFormValues({ ...formValues, email: e.target.value })
+        }
+      />
+
+      <label htmlFor="Pseudonyme">Pseudonyme</label>
+      <input
+        className="form-input"
+        type="text"
+        id="Pseudonyme"
+        name="Pseudonyme"
+        value={formValues.pseudo}
+        onChange={(e) =>
+          setFormValues({ ...formValues, pseudo: e.target.value })
+        }
+      />
+      <label htmlFor="Mot de passe">Mot de passe</label>
+      <input
+        className="form-input"
+        type="password"
+        id="Mot de passe"
+        name="Mot de passe"
+        value={formValues.password}
+        onChange={(e) =>
+          setFormValues({ ...formValues, password: e.target.value })
+        }
+      />
+      <label htmlFor="Confirmation du mot de passe">
+        Confirmation du mot de passe
+      </label>
+      <input
+        className="form-input"
+        type="password"
+        id="Confirmation du mot de passe"
+        name="Confirmation du mot de passe"
+        value={formValues.passwordConfirm}
+        onChange={(e) =>
+          setFormValues({ ...formValues, passwordConfirm: e.target.value })
+        }
+      />
+      <button onClick={() => setStepCounter(stepCounter + 1)}>Suivant</button>
     </div>
-  );
+  ) : stepCounter === 2 ? (
+    <div
+      style={{
+        backgroundColor: "red",
+        height: "800px",
+        display: "flex",
+        flexDirection: "column",
+        width: "800px",
+        alignItems: "flex-start",
+      }}
+    >
+      <label htmlFor="Adresse">Adresse</label>
+      <input
+        className="form-input"
+        type="text"
+        id="Adresse"
+        name="Adresse"
+        value={formValues.address.street}
+        onChange={(e) =>
+          setFormValues({
+            ...formValues,
+            address: { ...formValues.address, street: e.target.value },
+          })
+        }
+      />
+      <label htmlFor="Ville">Ville</label>
+      <input
+        className="form-input"
+        type="text"
+        id="Ville"
+        name="Ville"
+        value={formValues.address.city}
+        onChange={(e) =>
+          setFormValues({
+            ...formValues,
+            address: { ...formValues.address, city: e.target.value },
+          })
+        }
+      />
+      <label htmlFor="Code postal">Code postal</label>
+      <input
+        className="form-input"
+        type="text"
+        id="Code postal"
+        name="Code postal"
+        value={formValues.address.zipCode}
+        onChange={(e) =>
+          setFormValues({
+            ...formValues,
+            address: { ...formValues.address, zipCode: e.target.value },
+          })
+        }
+      />
+      <button onClick={() => setStepCounter(stepCounter - 1)}>Précédent</button>
+      <button onClick={() => createUser()}>Validez</button>
+    </div>
+  ) : null;
 };
 
 export default Signup;
